d0807c5bc27a524ba60a77dd2856cd83aeeb2fce
reinhardjs <reinhardjsilalahi@gmail.com>
2022-10-28 03:11:27 +0700

    added changes 2

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ var logger = require('morgan');
 
 const app = express();
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 app.get("/", (req, res) => {
-    res.send("Yo wassup, this is an api build with express, whoose v2!");
+    res.send("Yo wassup, this is an api build with express, whoose v3!");
 });
 
 app.listen(1234);
