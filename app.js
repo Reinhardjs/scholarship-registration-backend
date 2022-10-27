741db0231f9d553bb7fa69c6831d61df127568cc
reinhardjs <reinhardjsilalahi@gmail.com>
2022-10-28 03:10:03 +0700

    added changes 1

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Yo wassup, this is an api build with express, whoos!");
+    res.send("Yo wassup, this is an api build with express, whoose v2!");
 });
 
 app.listen(1234);
