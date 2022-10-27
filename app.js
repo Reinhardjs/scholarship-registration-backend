650b443432a34a28fac1aabccdb4b330fa9fafdd
reinhardjs <reinhardjsilalahi@gmail.com>
2022-10-28 03:05:45 +0700

    try

--- app.js
+++ app.js
@@ -3,19 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.get("/", (req, res) => {
+    res.send("Yo wassup, this is an api build with express, whoos!");
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
 app.listen(1234);
