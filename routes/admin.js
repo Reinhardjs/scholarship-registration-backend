01c5cabaeadbd7a816bcc0c73cd187b5dafebc23
obeman <naufalmuhammadroshandi@gmail.com>
2022-11-16 15:54:13 +0700

    create change-password, download-excel for both tables service, view-all tables

--- routes/admin.js
+++ routes/admin.js
@@ -7,10 +7,11 @@ const fs = require('fs');
 
 const { user } = require('../models');
 const { teacherTraining } = require('../models');
+const { japaneseStudies } = require('../models');
 
 
 
-/* GET home page. */
+/* post home page. */
 router.post('/login', async(req, res, next) =>{
 	let username = req.body.username;
 	let password = req.body.password;
@@ -32,27 +33,64 @@ router.post('/login', async(req, res, next) =>{
 	}
 });
 
-router.get('/logout', async(req, res, next) =>{
+router.post('/change-password', async(req, res, next) =>{
+	let username = req.session.username;
+	let password = req.body.password;
+	if (req.session.loggedin) {
+    const authUser = await user.findOne({ where: { 
+      username: username
+     }});
+
+     authUser.password = password;
+     await authUser.save();
+     res.send('Successfully Changed Password');
+	}else{
+    res.send('Please login to view this page!');
+}
+});
+
+router.post('/logout', async(req, res, next) =>{
 	req.session.destroy((err) => {
     if(err) {
         return console.log(err);
     }
     // Hapus cokie yang masih tertinggal
     res.clearCookie('secretname');
-    res.send('Berhasil Logout');
+    res.send('Logout Successful');
   });
 });
 
-router.get('/title', (req,res) => {
-  res.json({
-    message: 'scholarship index japan mext'
-  })
+router.post('/teacher-training/view', async(req, res, next) =>{
+	if (req.session.loggedin) {
+    const users = await teacherTraining.findAll({order: [['id', 'ASC']]});
+    const count = await teacherTraining.count();
+    res.json({
+      count: count,
+      data: users
+    });
+	}else{
+    res.send('Please login to view this page!');
+}
 });
 
-router.get('/teacher-training/download-excel', async (req,res) => {
+router.post('/japanese-studies/view', async(req, res, next) =>{
+	if (req.session.loggedin) {
+    const users = await japaneseStudies.findAll({order: [['id', 'ASC']]});
+    const count = await japaneseStudies.count();
+    res.json({
+      count: count,
+      data: users
+    });
+	}else{
+    res.send('Please login to view this page!');
+}
+});
+
+
+router.post('/teacher-training/download-excel', async (req,res) => {
   // Find all users
   if(req.session.loggedin){
-      const users = await teacherTraining.findAll();
+      const users = await teacherTraining.findAll({order: [['id', 'ASC']]});
       console.log(users.every(user => user instanceof teacherTraining)); // true
       
       const rows = users.map(row => ({
@@ -100,6 +138,62 @@ router.get('/teacher-training/download-excel', async (req,res) => {
           XLSX.writeFile(workbook, "downloads/teacherTraining.xlsx", { compression: true });
       
           res.download("downloads/teacherTraining.xlsx");
+          
+      } catch (error) {
+          console.log(error.message);
+          throw error;
+      }
+  }else{
+      res.send('Please login to view this page!');
+  }
+  
+});
+
+router.post('/japanese-studies/download-excel', async (req,res) => {
+  // Find all users
+  if(req.session.loggedin){
+      const users = await japaneseStudies.findAll({order: [['id', 'ASC']]});
+      console.log(users.every(user => user instanceof teacherTraining)); // true
+      
+      const rows = users.map(row => ({
+          testId: row.testId,
+          name: row.name,
+          gender: row.gender,
+          birthdate: row.birthdate,
+          japaneseResident: row.japaneseResident,
+          province: row.province,
+          city: row.city,
+          address: row.address,
+          telephone: row.telephone,
+          handphone: row.handphone,
+          email: row.email,
+          university: row.university,
+          semester: row.semester,
+          ipk: row.ipk,
+          jlpt: row.jlpt,
+          jlptScore: row.jlptScore,
+          testLocation: row.testLocation
+      }));
+  
+      const worksheet = XLSX.utils.json_to_sheet(rows);
+      const header = ["Test Number","Name", "Gender", "Birthdate", "Last Education", "Province", "City", "Address", "Telephone", "Handphone", "Email", "University"
+      , "Major", "IPK" , "JLPT", "JLPT Score", "Test Location"];
+  
+      XLSX.utils.sheet_add_aoa(worksheet, [header], { origin: "A1" });
+  
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, worksheet, "Dates");
+  
+      const downloadFolder = path.resolve(__dirname, "../downloads");
+      
+      if (!fs.existsSync(downloadFolder)) {
+          fs.mkdirSync(downloadFolder);
+      }
+      try {
+  
+          XLSX.writeFile(workbook, "downloads/japaneseStudies.xlsx", { compression: true });
+      
+          res.download("downloads/japaneseStudies.xlsx");
           
       } catch (error) {
           console.log(error.message);
